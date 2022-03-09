88fd0e6941ae94d8fd7d26fee21ceb74128d14f5
Mary78234 <wang11quanquan@gmail.com>
2022-03-09 15:33:17 +0100

    advice service

---
+++ CoreApp/src/app/advice.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdviceService {
+
+  constructor(private http: HttpClient) { }
+
+  getAdvice() {
+    return this.http.get('https://api.adviceslip.com/advice');
+  }
+}
--- CoreApp/src/app/app.component.ts
+++ CoreApp/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdviceService } from './advice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CoreApp';
+  advice!: any;
+  constructor(private adviceService: AdviceService){
+  }
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  private load(){
+    this.adviceService.getAdvice().subscribe(res =>{
+      if(res){
+        this.advice = res;
+      } else {
+        alert("Advice not found");
+      }
+    })
+  }
 }
